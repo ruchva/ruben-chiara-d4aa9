645e93affadb9314bc644735afd3247b78e6a13a
Stackbit Code Editor <projects@stackbit.com>
2021-07-01 21:42:18 +0000

    index.js: changed

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,7 @@
 import advanced from './advanced';
 import blog from './blog';
 import page from './page';
-import portfolio from './podcast';
+import portfolio from './portfolio';
 import post from './post';
 import project from './project';
 
